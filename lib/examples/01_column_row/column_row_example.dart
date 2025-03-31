826485a77edb01a25b169a8f655c98f2cbc83573
Kim <fopc4131@gmail.com>
2025-03-31 19:58:15 +0900

    feat: Column, Row 예제 추가

---
+++ lib/examples/01_column_row/column_row_example.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ColumnRowExample extends StatelessWidget {
+  const ColumnRowExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Column(
+          // mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Text("Row 1"),
+                Text("Row 2"),
+                Text("Row 3"),
+                Text("Row 4"),
+              ],
+            ),
+            Column(
+              children: [
+                Text("Column 1"),
+                Text("Column 2"),
+                Text("Column 3"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice_examples/examples/01_column_row/column_row_example.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: ExampleSelector(),
+    );
+  }
+}
+
+class ExampleSelector extends StatelessWidget {
+  final List<_ExampleItem> examples = [
+    _ExampleItem(
+      title: '01. Column Row 예제',
+      widget: ColumnRowExample(),
+    ),
+    //_ExampleItem(title: '02. Column/Row 예제', widget: ColumnRowExample()),
+    //_ExampleItem(title: '03. Stateful Counter', widget: CounterExample()),
+  ];
+
+  ExampleSelector({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter 예제 셀렉터')),
+      body: ListView.builder(
+        itemCount: examples.length,
+        itemBuilder: (context, index) {
+          final example = examples[index];
+          return ListTile(
+            title: Text(example.title),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => example.widget),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _ExampleItem {
+  final String title;
+  final Widget widget;
+
+  _ExampleItem({required this.title, required this.widget});
+}
