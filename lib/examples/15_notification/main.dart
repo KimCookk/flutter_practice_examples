154d18fafb0c912e1b41986889586dfd5ddcbb74
Kim <fopc4131@gmail.com>
2025-04-23 21:45:01 +0900

    feat(15.notification): 로컬 스케줄  알림 기능 추가

--- lib/examples/15_notification/main.dart
+++ lib/examples/15_notification/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_examples/examples/15_notification/notificationService.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  tz.initializeTimeZones();
   await Notificationservice.initialize();
   await requestNotificationPermission();
+
   runApp(NotificationApp());
 }
 
@@ -40,12 +43,44 @@ class HomePage extends StatelessWidget {
         title: Text("Notification App"),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Notificationservice.showNotification(
-                title: '테스트', body: '로컬 알림 테스트!');
-          },
-          child: Text('Push Notification'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Notificationservice.showNotification(
+                    title: '테스트', body: '로컬 알림 테스트!');
+              },
+              child: Text('Show Push Notification'),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final now = DateTime.now();
+                final scheduled = now.add(const Duration(seconds: 5));
+
+                Notificationservice.showScheduledNotification(
+                  title: '예약 알림',
+                  body: '10초 뒤에 도착하는 알림',
+                  scheduledTime: scheduled,
+                );
+
+                debugPrint("📅 알림 예약됨: ${scheduled.toLocal()}");
+              },
+              child: Text('After 10s Push Notification'),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Notificationservice.printScheduledNotificaiton();
+              },
+              child: Text('Show Push Notification List'),
+            ),
+          ],
         ),
       ),
     );
--- lib/examples/15_notification/notificationService.dart
+++ lib/examples/15_notification/notificationService.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
 
 class Notificationservice {
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
@@ -35,5 +36,45 @@ class Notificationservice {
       body,
       platformDetails,
     );
+  }
+
+  static Future<void> showScheduledNotification({
+    required String title,
+    required String body,
+    required DateTime scheduledTime,
+  }) async {
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+      'default_channel_id',
+      '기본채널',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    const NotificationDetails platformDetails = NotificationDetails(
+      android: androidDetails,
+    );
+
+    await _notificationsPlugin.zonedSchedule(
+      DateTime.now().millisecondsSinceEpoch ~/ 1000,
+      title,
+      body,
+      tz.TZDateTime.from(scheduledTime, tz.local),
+      platformDetails,
+      androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
+      payload: '알림 페이로드',
+    );
+  }
+
+  static Future<void> printScheduledNotificaiton() async {
+    final List<PendingNotificationRequest> pendingList =
+        await FlutterLocalNotificationsPlugin().pendingNotificationRequests();
+
+    for (var notif in pendingList) {
+      print('알림 ID: ${notif.id}');
+      print('제목: ${notif.title}');
+      print('내용: ${notif.body}');
+      print('페이로드: ${notif.payload}');
+    }
   }
 }
