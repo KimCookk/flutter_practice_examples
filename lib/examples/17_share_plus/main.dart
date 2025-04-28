35ec65edf93c4570e84435497f801837de1613cb
Kim <fopc4131@gmail.com>
2025-04-28 11:04:21 +0900

    feat(17.share_plus): Firebase remote config 활용 초기 설정 테스트 및 설정 파일 .gitignore 추가

--- lib/examples/17_share_plus/main.dart
+++ lib/examples/17_share_plus/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(SharePlusApp());
 }
 
@@ -28,13 +32,41 @@ class HomePage extends StatelessWidget {
         title: Text('Share Plus Example'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Share.share('앱 링크: https://yourapp.com');
-          },
-          child: Icon(Icons.ios_share),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Share.share('앱 링크: https://yourapp.com');
+              },
+              child: Icon(Icons.ios_share),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  final link = await fetchAppDownloadLink();
+                  if (link.isNotEmpty) {
+                    print('앱 링크 : $link');
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('❗ 링크를 가져오지 못했습니다')),
+                    );
+                  }
+                } catch (e) {
+                  print('에러 발생 : $e');
+                }
+              },
+              child: Text('Print Download Link'),
+            )
+          ],
         ),
       ),
     );
+  }
+
+  Future<String> fetchAppDownloadLink() async {
+    final remoteConfig = FirebaseRemoteConfig.instance;
+    await remoteConfig.fetchAndActivate();
+    return remoteConfig.getString('app_download_link');
   }
 }
